6a2451eb9b7c64f9598d4bbd0a17c0e2e807184a
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2022-12-14 02:08:41 -0300

    Feat: Add prop-types to user avatar

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,6 +1,14 @@
+import PropTypes from "prop-types";
+
 const UserAvatar = ({style, avatarUrl}) => {
     return (
         <img style={style} src={avatarUrl} alt="user-avatar"/>
     )
 }
+
+UserAvatar.propTypes = {
+    style: PropTypes.object.isRequired,
+    avatarUrl: PropTypes.string.isRequired
+};
+
 export default UserAvatar;
